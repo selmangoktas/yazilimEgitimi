d88619560661b7acac34d5be8613dca50d1272ff
selman <selman@karatekin.edu.tr>
2021-09-08 10:15:10 +0300

    egitim yeni cevap eklemeleri

---
+++ ciftSayilariBulan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ciftSayilariBulan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci Sayıyı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int bolum = 0;
+            int sonuc =0;
+            for (int i = 0; i <= sayi; i++)
+            {
+                if (((i / 2) * 2) == i)
+                    sonuc = sonuc + i;
+            }
+
+            Console.WriteLine($"0 ile {sayi} arasindaki çift sayıların toplamı: {sonuc} ");
+        }
+    }
+}
---
+++ terstenYazanUygulama/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace terstenYazanUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayıyı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
